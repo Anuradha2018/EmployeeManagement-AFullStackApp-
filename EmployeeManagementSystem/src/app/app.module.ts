d98b07aa5be33a4408d182ac7fe9992b45cc044c
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-01-16 19:12:48 +0100

    create components created

--- EmployeeManagementSystem/src/app/app.module.ts
+++ EmployeeManagementSystem/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+
+const appRoutes: Routes = [
+  {
+    path:'list', component: ListEmployeesComponent
+  },
+  {
+    path:'create', component: CreateEmployeeComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
