a2f6061bcba22572d7940714f068bf42966310e6
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-01-18 17:43:05 +0100

    Backend For EMS created

--- EmployeeManagementSystem/src/app/employees/create-employee/create-employee.component.ts
+++ EmployeeManagementSystem/src/app/employees/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
+import { Department } from './../../models/department';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-employee',
@@ -7,7 +9,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
- 
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'R&D' },
+    { id: 5, name: 'Finance' },
+    { id: 6, name: 'Accounts' },
+    { id: 7, name: 'Sales&Marketing' },
+    { id: 8, name: 'Procurement' },
+
+  ];
   constructor() { }
 
   ngOnInit() {
