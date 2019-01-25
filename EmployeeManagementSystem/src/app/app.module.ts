c5b0dd74eae604c76eb387e50ca526dc94fc43f4
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-01-25 15:37:39 +0100

    Components formatted

--- EmployeeManagementSystem/src/app/app.module.ts
+++ EmployeeManagementSystem/src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { AuthService } from './services/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import {RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { AuthService } from "./services/auth.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
-import { AppComponent } from './app.component';
-import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
-import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
-import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { EmpdetailComponent } from './employees/empdetail/empdetail.component';
-import { RegisterUserComponent } from './register-user/register-user.component';
+import { AppComponent } from "./app.component";
+import { ListEmployeesComponent } from "./employees/list-employees/list-employees.component";
+import { CreateEmployeeComponent } from "./employees/create-employee/create-employee.component";
+import { LoginComponent } from "./login/login.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { EmpdetailComponent } from "./employees/empdetail/empdetail.component";
+import { RegisterUserComponent } from "./register-user/register-user.component";
 
 const appRoutes: Routes = [
-  { path:'empdetail/:id', component:EmpdetailComponent },
-  { path:'register', component: RegisterUserComponent },
-  { path:'login', component: LoginComponent },
-  { path:'list', component: ListEmployeesComponent },
-  { path:'create', component: CreateEmployeeComponent },
-  { path:'', redirectTo: '/login', pathMatch: 'full' }
-]
+  { path: "empdetail/:id", component: EmpdetailComponent },
+  { path: "register", component: RegisterUserComponent },
+  { path: "login", component: LoginComponent },
+  { path: "list", component: ListEmployeesComponent },
+  { path: "create", component: CreateEmployeeComponent },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+];
 
 @NgModule({
   declarations: [
@@ -31,16 +31,16 @@ const appRoutes: Routes = [
     LoginComponent,
     NavbarComponent,
     EmpdetailComponent,
-    RegisterUserComponent
+    RegisterUserComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
   ],
   providers: [AuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
